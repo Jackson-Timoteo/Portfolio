822a02aa8d20959b3f5887aa9c21b0409facc3bf
Jackson-Timoteo <jackson_siilva@hotmail.com>
2024-05-14 20:00:38 -0300

    add function for item

--- js/script.js
+++ js/script.js
@@ -17,5 +17,24 @@ function changeTheme() {
 toggleTheme.addEventListener("click", changeTheme)
 
 
+accordionHeaders.forEach(header =>{
+    header.addEventListener("click", () => {
+        const accordionItem = header.parentElement;
+        const accordionActive = accordionItem.classList.contains("active")
+        
+        
+        accordionActive ? accordionItem.classList.remove("active") 
+        : accordionItem.classList.add("active")
+    })
+})
 
-console.log(toggleTheme)
+menuLinks.forEach(item => {
+    item.addEventListener("click", () => {
+        menuLinks.forEach(i => i.classList.remove("active"));
+        item.classList.add("active");
+    })
+})
+
+
+
+console.log(accordionHeaders    )
